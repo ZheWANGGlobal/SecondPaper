8424cb462a15a11820e7f24e517bebe650328516
Zhe Wang <zhewangglobal@gmail.com>
2017-12-10 13:28:45 +0800

    modified the structure of folder FaceCrop in folder Preprocessing

---
+++ Preprocessing/FaceCrop/Crop_Face_Instructions.m
@@ -0,0 +1,4 @@
+%% run ImageLoad.m First
+%% run faceSave.m after run ImageLoad.m
+%% file face_segment.m will be invoked in faceSave.m
+%% divided face will be save in folder Divided Face
--- Preprocessing/FaceCrop/faceSave.m
+++ Preprocessing/FaceCrop/faceSave.m
@@ -1,6 +1,6 @@
 clear;
-stImageFilePath  = 'C:\Users\Andrew\Desktop\SecondPaper-master\';
-stImageSavePath  = 'C:\Users\Andrew\Desktop\SecondPaper-master\newPic\';
+stImageFilePath  = 'C:\Users\Andrew\Desktop\SecondPaper-master\Preprocessing\FaceCrop\OriginalDataset\';
+stImageSavePath  = 'C:\Users\Andrew\Desktop\SecondPaper-master\Preprocessing\FaceCrop\DividedFace\';
 dirImagePathList = dir(strcat(stImageFilePath,'*.png'));        %读取该文件夹下所有图片的路径（字符串格式）
 iImageNum        = length(dirImagePathList);                    %获取图片的总数量
 if iImageNum > 0                                                %批量读入图片，进行五官检测，再批量检测
