2f9752bab7be1cfac5372971883097ab1a529275
Zhe Wang <zhewangglobal@gmail.com>
2017-12-26 09:05:22 +0800

    deleted unused file folders and fix some bugs
    - deleted the former SVM folders and keep the Lin's Svm Library
    - fixed the bugs invoking the SVM tools
    - add confusion matrix analysis to judge tags that need to be enhanced
    - to do : using multiple levels of svm to enhance the performance of
    unrecognized expressions

---
+++ FaceExpession/subprogram/Dimension Reduction/SparseLocal_FDA.m
@@ -0,0 +1,103 @@
+function V=SparseLocal_FDA(XXX,Y,r,kNN,epsilon)
+%
+% 稀疏局部Fisher判别分析进行有监督降维度
+%       
+% 输入:
+%    X:      d 乘 n 原始样本矩阵     
+%            d --- 样本的维度（jaffe矩阵为64*64）
+%            n --- 样本的个数（jaffe为213）
+%    Y:      n 维样本标签集  
+%    r:      降维空间的维度（默认值d）
+%    kNN:    计算局部缩放邻接权值矩阵时要用到的最近邻参数(默认值 7)
+%    epsilon:torlence  
+% 输出:
+%    V: d 乘 r 投影矩阵 (Z=T'*X)
+%    Z: r 乘 n 降维后的样本矩阵 
+
+XXX = double(XXX);
+
+[d,n]=size(XXX);
+
+if nargin<4
+%    kNN=7;
+  kNN=1;
+end
+
+if nargin<3
+  r=d;
+end
+
+tSb=zeros(d,d);%类间离散度矩阵
+tSw=zeros(d,d);%类内离散度矩阵
+W = ones(n)./n;
+for c=1:max(Y)
+  Xc=XXX(:,Y==c);
+  Temp = find(Y==c);              
+  nc=size(Xc,2); % 矩阵Xc的列数赋给nc
+
+  % 计算权值矩阵A  LFDA采用局部缩放权值矩阵
+  Xc2=sum(Xc.^2,1);
+  distance2=repmat(Xc2,nc,1)+repmat(Xc2',1,nc)-2*Xc'*Xc;
+  [sorted,index]=sort(distance2);
+  kNNdist2=sorted(kNN+1,:);
+  sigma=sqrt(kNNdist2);
+  localscale=sigma'*sigma;
+  flag=(localscale~=0);
+  A=zeros(nc,nc);
+  A(flag)=exp(-distance2(flag)./localscale(flag));
+   
+  W(Temp,Temp)=A./n;
+  
+  Xc1=sum(Xc,2);
+  G=Xc*(repmat(sum(A,2),[1 d]).*Xc')-Xc*A*Xc';
+  tSb=tSb+G/n+Xc*Xc'*(1-nc/n)+Xc1*Xc1'/n;
+  tSw=tSw+G/nc;
+end
+
+%计算类间和类内离散度矩阵
+X1=sum(XXX,2);
+tSb=tSb-X1*X1'/n-tSw;
+
+tSb=(tSb+tSb')/2;
+tSw=(tSw+tSw')/2;
+
+Dlt = diag(sum(W));
+Llt = Dlt - W;
+[Qlt,Alpha]=eig(Llt);
+Hlt = XXX*Qlt*abs(Alpha^(1/2));
+[P,aaa,~]=svd(Hlt);  
+index=find(diag(aaa)>1.e-5);
+P=P(:,index);
+                                                                                    
+Sb = P'*tSb*P;
+Sw = P'*tSw*P;
+[U,D]=eig(Sb,Sw);
+[D,index]=sort(diag(D));%求以L为对角的对角矩阵
+U=U(:,index);
+index=find(D>1.e-5);%找到大于0的该矩阵的下标
+
+D=D(index(end-r+1:end));
+U=U(:,index(end-r+1:end));%有意义的特征值所对应的特征向量
+
+%线性bregman迭代进行稀疏处理
+% V = P*U;
+tq = size(U,2);
+%初始化相关变量
+% [n,d]=size(XXX);
+V = zeros(d,tq); B = zeros(d,tq);                                                                                         
+delta=.01; mu=.5;    % delta mu 作为参数 对比迭代得到的V和PU相乘的V
+if nargin<6
+    %epsilon=10^(-5);
+    epsilon=1;
+end
+%线性迭代算法
+k=1;
+ttemp(1) = norm(P'*V-U);
+while ttemp(k)>=epsilon && k<=10000 %循环判断
+    k=k+1;
+    B = B - P*(P'*V-U);
+    V = delta*shrink(B,mu);
+    ttemp(k) = norm(P'*V-U);
+end
+
+% Z=V'*XXX;X_tst = V'*x_tst;
--- FaceExpession/subprogram/test.m
+++ FaceExpession/subprogram/test.m
@@ -1,118 +1,187 @@
 clear
 % load Jaffe
-% load JaffeX_Six
-% load JaffeY_Six
-% load JaffeY_ThreeCate
 load x_trn
 load x_tst
 load y_tst
 load y_trn
-load y_three
-load y_three_tst
+load y_three    % 训练集的三大类别标签
+load y_three_tst   % 测试集的三大类别标签
 % clear Y1;
 % Y=Y1;
 N1=20;
 % X=double(X)./255;
+x_trn = double(x_trn)./255;
+x_tst = double(x_tst)./255;
 
 global p1 knn
  knn=1;
 p1=2^-8;
-C=100;
-ker='linear';
-ker='rbf';
-% for loop=1:50
-%  [x_trn,y_trn,x_tst,y_tst,trainindex,testindex]=sample_random(X,Y,N1);
-% [x_trn,y_trn,x_tst,y_tst,trainindex,testindex]=sample_random(JaffeX_Six,JaffeY_Six,N1);
-%  [~,P]=pca(x_trn,size(x_trn,1));
-%  newx_trn=x_trn*P;
-%  newx_tst=x_tst*P;
-%   [out]=cknear(knn,newx_trn,y_trn,newx_tst); 
-%   mean(out==y_tst)
- %% KNN
-   [out]=cknear(knn,x_trn,y_trn,x_tst); 
-%   Acc(loop,1)=mean(out==y_tst);
+%% using KNN directly
+[out]=cknear(knn,x_trn,y_trn,x_tst); 
 Acc(1,1)=mean(out==y_tst);
-  %% SVM
-%  [model,err,predictY,out]= SVC_lib_tt(x_trn,y_trn,ker,C,x_tst,y_tst);
-%    Acc(loop,2)=1-err;
-model = svmtrain(y_trn,x_trn,'-s 0 -t 0 -c 100');
+%% using SVM directly
+model = svmtrain(y_trn,x_trn,'-s 0 -t 2 -g 0.001 -c 100');
 [out, accu, de] = svmpredict(y_tst, x_tst, model);
-% Acc(loop,2)=mean(out==y_tst);
 Acc(1,2)=mean(out==y_tst);
-%% SVM new senario
-% % % predict three categories
-% % y_trn_three = JaffeY_ThreeCate(trainindex,:);
-% % model = svmtrain(y_trn_three,x_trn,'-s 0 -t 2 -c 100');
-% % y_tst_three = JaffeY_ThreeCate(testindex,:);
-% % [out, accu, de] = svmpredict(y_tst_three, x_tst, model);
-% % ind8 = find(out==8);
-% % ind9 = find(out == 9);
-% % ind10 = find(out == 10);
-% % 
-% % %retrain 
-% % ind_trn8 = find(y_trn==1.0|y_trn==2.0);
-% % ind_trn9 = find(y_trn==3.0|y_trn==5.0);
-% % ind_trn10 = find(y_trn==4.0|y_trn==6.0);
-% % model8 = svmtrain(y_trn(ind_trn8),x_trn(ind_trn8),'-s 0 -t 2 -c 100');% model 8
-% % model9 = svmtrain(y_trn(ind_trn9),x_trn(ind_trn9),'-s 0 -t 2 -c 100');
-% % model10 = svmtrain(y_trn(ind_trn10),x_trn(ind_trn10),'-s 0 -t 2 -c 100');
-% % 
-% % [out8, accu, de] = svmpredict(y_tst(ind8), x_tst(ind8), model8);
-% % Acc(loop,3)=mean(out8==y_tst(ind8));
-% % [out9, accu, de] = svmpredict(y_tst(ind9), x_tst(ind9), model9);
-% % Acc(loop,4)=mean(out9==y_tst(ind9));
-% % [out10, accu, de] = svmpredict(y_tst(ind10), x_tst(ind10), model10);
-% % Acc(loop,5)=mean(out10==y_tst(ind10));
 
-% save x_trn.mat x_trn
-% save y_trn.mat y_trn
-% save x_tst.mat x_tst
-% save y_tst.mat y_tst
+confusionMatrix = zeros(7,7);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ind_trn8 = find(y_trn==1|y_trn==2); 
-ind_trn9 = find(y_trn==3|y_trn==5);
-ind_trn10 = find(y_trn==4|y_trn==6);
-model8 = svmtrain(y_trn(ind_trn8),x_trn(ind_trn8),'-s 0 -t 2 -c 100');% model 8
-model9 = svmtrain(y_trn(ind_trn9),x_trn(ind_trn9),'-s 0 -t 0 -c 100');
-model10 = svmtrain(y_trn(ind_trn10),x_trn(ind_trn10),'-s 0 -t 0 -c 100');
-% predict three categories
-model = svmtrain(y_three,x_trn,'-s 0 -t 0 -c 100');
-[out_three, accu, de] = svmpredict(y_three_tst, x_tst, model);
-ind8 = find(out_three==8);
-ind9 = find(out_three == 9);
-ind10 = find(out_three == 10);
+for i=1:63
+        confusionMatrix(out(i),y_tst(i))=confusionMatrix(out(i),y_tst(i))+1;
+end
 
-[out8, accu, de] = svmpredict(y_tst(ind8), x_tst(ind8), model8);
-Acc(1,3)=mean(out8==y_tst(ind8));
-[out9, accu, de] = svmpredict(y_tst(ind9), x_tst(ind9), model9);
-Acc(1,4)=mean(out9==y_tst(ind9));
-[out10, accu, de] = svmpredict(y_tst(ind10), x_tst(ind10), model10);
-Acc(1,5)=mean(out10==y_tst(ind10));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 4 5作为一个大类训练
 
 
 
-%% SMFA + SVM
-   % sparse MFA without PCA
-%      for i=1:20
-%         V_SMFA_PCA=sparse_MFA(x_trn,y_trn,i); 
-%         x_trn = double(x_trn);
-%         x_tst= double(x_tst);
-%         X_trn=x_trn*V_SMFA_PCA;
-%         X_tst=x_tst*V_SMFA_PCA;
-%         [out2]=cknear(knn,X_trn,y_trn,X_tst); 
-%         res2(loop,i)=mean(out2==y_tst);
-%      end
-% % x_trn = double(x_trn);
-% % x_tst= double(x_tst);
-% % V_SMFA=sparse_MFA(x_trn,y_trn,10); 
-% % X_trn=x_trn*V_SMFA;
-% % X_tst=x_tst*V_SMFA;
-% % model2 = svmtrain(y_trn,X_trn,'-s 0 -t 2 -c 100');
-% % [out, accu, de] = svmpredict(y_tst, X_tst, model2);
-% % Acc(loop,3)=mean(out==y_tst);
-% end
-% mean(Acc)
-% Acc
+
+
+ %% SVM new senario with two levels （未降维）
+% % 训练第二层的三个分类器   第一大类记为类别8，第二大类记为类别9，第三大类记为类别10
+% ind_trn8 = find(y_trn==1|y_trn==2); 
+% ind_trn9 = find(y_trn==3|y_trn==5);
+% ind_trn10 = find(y_trn==4|y_trn==6);
+% model8 = svmtrain(y_trn(ind_trn8),x_trn(ind_trn8,:),'-s 0 -t 2 -g 0.01 -c 100');
+% model9 = svmtrain(y_trn(ind_trn9),x_trn(ind_trn9,:),'-s 0 -t 2 -g 0.01 -c 100');
+% model10 = svmtrain(y_trn(ind_trn10),x_trn(ind_trn10,:),'-s 0 -t 2 -g 0.01 -c 100');
+% 
+% 
+% 
+% % 第一层分类器训练及预测
+% model = svmtrain(y_three,x_trn,'-s 0 -t 2 -g 0.01 -c 100');
+% [out_three, accu, de] = svmpredict(y_three_tst, x_tst, model);
+% 
+% %找出第一层预测结果，准备分别投入相应分类器进行第二层预测
+% ind8 = find(out_three==8);
+% ind9 = find(out_three == 9);
+% ind10 = find(out_three == 10);
+% 
+% % 第二层预测y_tst(ind8)
+% [out8, accu, de] = svmpredict(y_tst(ind8), x_tst(ind8,:), model8);
+% Acc(1,3)=mean(out8==y_tst(ind8));
+% [out9, accu, de] = svmpredict(y_tst(ind9), x_tst(ind9,:), model9);
+% Acc(1,4)=mean(out9==y_tst(ind9));
+% [out10, accu, de] = svmpredict(y_tst(ind10), x_tst(ind10,:), model10);
+% Acc(1,5)=mean(out10==y_tst(ind10));
+
+%% SVM new senario with two levels （PCA降维）
+% % 先用PCA对训练集和测试集进行降维
+% [eigvectorPCA,eigvaluePCA] = PCA2(double(x_trn),3974);
+% x_trn_PCA = double(x_trn)*eigvectorPCA;
+% x_tst_PCA = double(x_tst)*eigvectorPCA; 
+% 
+% % 训练第二层的三个分类器   第一大类记为类别8，第二大类记为类别9，第三大类记为类别10
+% ind_trn8 = find(y_trn==1|y_trn==2); 
+% ind_trn9 = find(y_trn==3|y_trn==5);
+% ind_trn10 = find(y_trn==4|y_trn==6);
+% model8 = svmtrain(y_trn(ind_trn8),x_trn_PCA(ind_trn8),'-s 0 -t 2 -c 100');
+% model9 = svmtrain(y_trn(ind_trn9),x_trn_PCA(ind_trn9),'-s 0 -t 2 -c 100');
+% model10 = svmtrain(y_trn(ind_trn10),x_trn_PCA(ind_trn10),'-s 0 -t 2 -c 100');
+% 
+% % 第一层分类器训练及预测
+% model = svmtrain(y_three,x_trn_PCA,'-s 0 -t 0 -c 100');
+% [out_three, accu, de] = svmpredict(y_three_tst, x_tst_PCA, model);
+% 
+% %找出第一层预测结果，准备分别投入相应分类器进行第二层预测
+% ind8 = find(out_three==8);
+% ind9 = find(out_three == 9);
+% ind10 = find(out_three == 10);
+% 
+% 
+% % 第二层预测
+% [out8, accu, de] = svmpredict(y_tst(ind8), x_tst_PCA(ind8), model8);
+% Acc(1,3)=mean(out8==y_tst(ind8));
+% [out9, accu, de] = svmpredict(y_tst(ind9), x_tst_PCA(ind9), model9);
+% Acc(1,4)=mean(out9==y_tst(ind9));
+% [out10, accu, de] = svmpredict(y_tst(ind10), x_tst_PCA(ind10), model10);
+% Acc(1,5)=mean(out10==y_tst(ind10));
+
+%% SVM new senario with two levels （using LDA）
+% %%% 第一层分类器训练及预测
+% % % 第一层降维
+% % [V_LDA_three,inde] = LDA(x_trn, y_three,6);
+% % x_trn_fistLevelDimensionReduction=x_trn*V_LDA_three;
+% % x_tst_fistLevelDimensionReduction=x_tst*V_LDA_three;
+% % 训练及预测
+% model = svmtrain(y_three,x_trn,'-s 0 -t 0 -c 100');
+% [out_three, accu, de] = svmpredict(y_three_tst, x_tst, model);
+% 
+% %%% 找出第一层预测结果，准备分别投入相应分类器进行第二层预测
+% ind8 = find(out_three==8);
+% ind9 = find(out_three == 9);
+% ind10 = find(out_three == 10);
+% 
+% %%% 训练第二层的三个分类器   第一大类记为类别8，第二大类记为类别9，第三大类记为类别10
+% ind_trn8 = find(y_trn==1|y_trn==2); 
+% ind_trn9 = find(y_trn==3|y_trn==5);
+% ind_trn10 = find(y_trn==4|y_trn==6);
+% %三次分别降维
+% [V_LDA_8,inde] = LDA(x_trn(ind_trn8), y_trn(ind_trn8),1);
+% x_trn_in8=x_trn(ind_trn8)*V_LDA_8;
+% X_tst_in8=x_tst(ind8)*V_LDA_8;
+% 
+% [V_LDA_9,inde] = LDA(x_trn(ind_trn9), y_trn(ind_trn9),1);
+% x_trn_in9=x_trn(ind_trn9)*V_LDA_9;
+% X_tst_in9=x_tst(ind9)*V_LDA_9;
+% 
+% [V_LDA_10,inde] = LDA(x_trn(ind_trn10), y_trn(ind_trn10),1);
+% x_trn_in10=x_trn(ind_trn10)*V_LDA_10;
+% X_tst_in10=x_tst(ind10)*V_LDA_10;
+% 
+% % 三个分类器的训练
+% model8 = svmtrain(y_trn(ind_trn8),x_trn_in8,'-s 0 -t 2 -c 100');
+% model9 = svmtrain(y_trn(ind_trn9),x_trn_in9,'-s 0 -t 2 -c 100');
+% model10 = svmtrain(y_trn(ind_trn10),x_trn_in10,'-s 0 -t 2 -c 100');
+% 
+% %%% 第二层预测
+% [out8, accu, de] = svmpredict(y_tst(ind8), x_tst(ind8), model8);
+% Acc(1,3)=mean(out8==y_tst(ind8));
+% [out9, accu, de] = svmpredict(y_tst(ind9), x_tst(ind9), model9);
+% Acc(1,4)=mean(out9==y_tst(ind9));
+% [out10, accu, de] = svmpredict(y_tst(ind10), x_tst(ind10), model10);
+% Acc(1,5)=mean(out10==y_tst(ind10));
+
+%% SVM new senario with two levels （using SMFA）
+% %%% 第一层分类器训练及预测
+% % 第一层降维
+% V_SMFA_three = sparse_MFA(x_trn, y_three,20);
+% x_trn_fistLevelDimensionReduction=x_trn*V_SMFA_three;
+% x_tst_fistLevelDimensionReduction=x_tst*V_SMFA_three;
+% % 训练及预测
+% model = svmtrain(y_three,x_trn,'-s 0 -t 0 -c 100');
+% [out_three, accu, de] = svmpredict(y_three_tst, x_tst, model);
+% 
+% %%% 找出第一层预测结果，准备分别投入相应分类器进行第二层预测
+% ind8 = find(out_three==8);
+% ind9 = find(out_three == 9);
+% ind10 = find(out_three == 10);
+% 
+% %%% 训练第二层的三个分类器   第一大类记为类别8，第二大类记为类别9，第三大类记为类别10
+% ind_trn8 = find(y_trn==1|y_trn==2); 
+% ind_trn9 = find(y_trn==3|y_trn==5);
+% ind_trn10 = find(y_trn==4|y_trn==6);
+% %三次分别降维
+% V_SMFA_8 = sparse_MFA(x_trn(ind_trn8), y_trn(ind_trn8),20);
+% x_trn_in8=x_trn(ind_trn8)*V_SMFA_8;
+% X_tst_in8=x_tst(ind8)*V_SMFA_8;
+% 
+% V_SMFA_9 = sparse_MFA(x_trn(ind_trn9), y_trn(ind_trn9),20);
+% x_trn_in9=x_trn(ind_trn9)*V_SMFA_9;
+% X_tst_in9=x_tst(ind9)*V_SMFA_9;
+% 
+% V_SMFA_10 = sparse_MFA(x_trn(ind_trn10), y_trn(ind_trn10),20);
+% x_trn_in10=x_trn(ind_trn10)*V_SMFA_10;
+% X_tst_in10=x_tst(ind10)*V_SMFA_10;
+% 
+% % 三个分类器的训练
+% model8 = svmtrain(y_trn(ind_trn8),x_trn_in8,'-s 0 -t 2 -c 100');
+% model9 = svmtrain(y_trn(ind_trn9),x_trn_in9,'-s 0 -t 2 -c 100');
+% model10 = svmtrain(y_trn(ind_trn10),x_trn_in10,'-s 0 -t 2 -c 100');
+% 
+% %%% 第二层预测
+% [out8, accu, de] = svmpredict(y_tst(ind8), x_tst(ind8), model8);
+% Acc(1,3)=mean(out8==y_tst(ind8));
+% [out9, accu, de] = svmpredict(y_tst(ind9), x_tst(ind9), model9);
+% Acc(1,4)=mean(out9==y_tst(ind9));
+% [out10, accu, de] = svmpredict(y_tst(ind10), x_tst(ind10), model10);
+% Acc(1,5)=mean(out10==y_tst(ind10));
